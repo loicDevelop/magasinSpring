d741f13cd0e44fbcf57f251a0f42a7b1b221ff9f
Loïc <loic.crbn@outlook.com>
2021-06-15 12:02:26 +0200

    dependance refaite avec Spring initializr

---
+++ src/test/java/com/example/magasinSpring/MagasinSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.magasinSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagasinSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
